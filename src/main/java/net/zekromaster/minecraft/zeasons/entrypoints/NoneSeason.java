719cda2e29b9a6a53e158b524d965cbf452dff66
Zekromaster <dev@zekromaster.net>
2024-10-23 21:32:06 +0200

    chore: Remove unused imports

--- src/main/java/net/zekromaster/minecraft/zeasons/entrypoints/NoneSeason.java
+++ src/main/java/net/zekromaster/minecraft/zeasons/entrypoints/NoneSeason.java
@@ -1,8 +1,6 @@
 package net.zekromaster.minecraft.zeasons.entrypoints;
 
 import net.mine_diver.unsafeevents.listener.EventListener;
-import net.minecraft.block.Block;
-import net.modificationstation.stationapi.api.client.event.color.block.BlockColorsRegisterEvent;
 import net.zekromaster.minecraft.zeasons.events.SeasonRegistryEvent;
 
 import static net.zekromaster.minecraft.zeasons.Zeasons.NO_SEASON;
